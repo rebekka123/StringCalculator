973c1f5b9d8237f955b0a2cb927f4087f9ce398c
rebekka123 <rebekkagudmunds@gmail.com>
2016-10-14 01:44:52 +0200

    Created second test: testOneNumber()

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -13,5 +13,10 @@ public class CalculatorTest {
 	public void testEmptyString() {
 		assertEquals(0, Calculator.add(""));
 	}
+	
+	@Test
+	public void testOneNumber() {
+		assertEquals(1, Calculator.add("1"));
+	}
 
 }
